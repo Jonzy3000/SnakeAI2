80a15313970a842ad1ed82c52f83f43cf054bf7a
Will <will3536@hotmail.co.uk>
2018-03-24 17:05:10 +0000

    Added a neural network to the brain

--- src/AI/brain.ts
+++ src/AI/brain.ts
@@ -2,12 +2,52 @@ import AISetup from "./setup"
 
 import GameInfo from "./../Game/gameInfo"
 
-export default class Brain {
-    constructor() {
+import Direction from "./../Game/Snake/direction"
 
+export default class Brain {
+    
+    private neuralNetwork:NeuralNetwork
+    
+    constructor(numInputs:number, numOutputs:number, numHiddenLayers:number, numNeuronsPerHiddenLayer:number, weights:number[]) {
+        this.neuralNetwork = new NeuralNetwork(numInputs,numOutputs,numHiddenLayers,numNeuronsPerHiddenLayer)    
+        this.neuralNetwork.setWeights(weights)
     }
 
     public onGameUpdate(input: GameInfo) {
+        let outputs:number[] = this.neuralNetwork.update()
 
+
+        return this.generateDirection(outputs)
     }
+
+    private generateDirection(neuralNetworkOutput:number[]): Direction{
+        var newDirection: Direction
+
+        let max:number = neuralNetworkOutput[0];
+        let maxIndex:number = 0;
+    
+        for (var i = 1; i < neuralNetworkOutput.length; i++) {
+            if (neuralNetworkOutput[i] > max) {
+                maxIndex = i;
+                max = neuralNetworkOutput[i];
+            }
+        }
+
+        switch(maxIndex) {
+            case 0:
+                newDirection = Direction.DOWN()
+                break;
+            case 1:
+                newDirection = Direction.UP()
+                break;
+            case 2:
+                newDirection = Direction.LEFT()
+                break;
+            case 3:
+                newDirection = Direction.RIGHT()
+                break;
+            default:        
+        }
+        return newDirection
+    } 
 }
