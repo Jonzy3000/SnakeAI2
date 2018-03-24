6c09d3dc814a4af1765a2fdd43137f0f2206f7b7
Ben Marsden <benmarsden@Bens-MacBook-Pro.local>
2018-03-24 18:14:49 +0000

    Added check to see if brains have finished their games.

--- src/AI/geneticAlgo.ts
+++ src/AI/geneticAlgo.ts
@@ -5,16 +5,43 @@ import Game from "./../Game/game";
 import Brain from "./brain";
 
 export default class GeneticAlgorithm {
+	private numInputs : number = 4;
+	private numHiddenLayers : number = 2;
+	private numNeurons : number = 4;
+	private generationCount : number = 0;
+	private fittest : number = 0;
+	private brainsFinished : boolean;
+	private brains : Brain[];
+	private weights : number[][];
     constructor(private app: PIXI.Application, private numberOfSnakes: number) {
     	this.startGeneration();
     }
 
     //Instatiate the brains 
     public startGeneration() {
+    	
+    	console.log("Generation: " + this.generationCount + " Fittest: " + this.fittest);
         for (let i = 0; i < this.numberOfSnakes; i++) {
-            new Game(this.app, [new Brain().onGameUpdate]);
+        	this.brains[i] = new Brain(this.numInputs, this.numHiddenLayers, this.numNeurons, this.weights[i]);
+            new Game(this.app, [this.brains[i].onGameUpdate]);
         }
+
+        setInterval(this.areBrainsFinished, 5000);
+        console.log("Are brains still running: " + this.brainsFinished);
     }
+
+    public areBrainsFinished() {
+    	for (let i = 0; i < this.numberOfSnakes; i++) {
+    		if (!this.brains[i].getIsGameFinished()) {
+    			this.brainsFinished = false;
+    			return false;
+    		}
+    	}
+    }
+
+
+
+
 
 //fitness function: score squared/time
 //todo: Every 5 seconds: are you all finished? I.e. we need some way of checking if all done.
